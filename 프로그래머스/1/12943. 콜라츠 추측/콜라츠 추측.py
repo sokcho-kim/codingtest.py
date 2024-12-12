cb8ccdd966acb50311c956a03c3687e276e3d141
sokchokim <82491728+sokcho-kim@users.noreply.github.com>
2024-12-12 17:15:56 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.19 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -1,14 +1,3 @@
 def solution(num):
     answer = 0
-    while num != 1 : 
-        if num % 2 == 0 : 
-            num = int(num / 2)
-        elif num % 2 == 1 : 
-            num = int(num * 3 + 1) 
-        else : 
-            if num == 1 :
-                return answer
-        answer += 1 
-    if answer > 500 : 
-        answer = -1
     return answer
